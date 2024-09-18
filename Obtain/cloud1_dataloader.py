f07053c1f5045acc167db4969f855b6bf56a0ba1
Adwait Kulkarni <adk1361@bu.edu>
2024-09-18 18:02:31 -0400

    Update cloud1_dataloader.py

--- Obtain/cloud1_dataloader.py
+++ Obtain/cloud1_dataloader.py
@@ -69,6 +69,70 @@ def get_dataloader(data_dir, batch_size, num_workers=4):
     )'''
 
 
+# import glob
+# import numpy as np
+# import torch
+# from torchvision import transforms
+# from torchvision.io import read_image
+# from torch.utils.data import Dataset
+# import os
+# import json
+
+# class CarlaDataset(Dataset):
+#     def __init__(self, data_dir):
+#         self.img_list = []
+#         self.depth_list = []
+#         self.data_list = []
+#         self.data_dir = data_dir
+
+#         # Traverse through subdirectories
+#         for run_dir in glob.glob(os.path.join(self.data_dir, 'rc_data', 'run_*')):
+#             # Load RGB images and Depth images
+#             rgb_dir = os.path.join(run_dir, 'rgb')
+#             depth_dir = os.path.join(run_dir, 'disparity')  # Assuming depth images are in a 'disparity' folder
+#             self.img_list += glob.glob(os.path.join(rgb_dir, '*.jpg'))
+#             self.depth_list += glob.glob(os.path.join(depth_dir, '*.png'))
+
+#             # Load JSON files for actions
+#             json_dir = os.path.join(run_dir, 'json')
+#             self.data_list += glob.glob(os.path.join(json_dir, '*.json'))
+
+#         print(f'Loaded {len(self.img_list)} images, {len(self.depth_list)} depth images, and {len(self.data_list)} action files.')
+
+#     def __len__(self):
+#         return len(self.data_list)
+
+#     def __getitem__(self, idx):
+#         # Load the JSON file
+#         with open(self.data_list[idx], 'r') as f:
+#             data = json.load(f)
+
+#         # Extract actions
+#         actions = torch.Tensor([data["Steer"], data["Throttle"]])
+
+#         # Load the corresponding RGB image
+#         img_path = self.img_list[idx]
+#         img = read_image(img_path)
+#         img = img[:3, 120:600, 400:880]  # Crop the image
+#         normalized_image = self.normalize(img.float() / 255.0)
+
+#         # Load the corresponding depth image
+#         depth_path = self.depth_list[idx]
+#         depth_img = read_image(depth_path)
+#         depth_img = depth_img.float() / 255.0  # Normalize depth if needed
+#         depth_img = depth_img.unsqueeze(0)  # Add channel dimension if necessary
+
+#         return normalized_image, depth_img, actions
+
+# def get_dataloader(data_dir, batch_size, num_workers=4):
+#     return torch.utils.data.DataLoader(
+#         CarlaDataset(data_dir),
+#         batch_size=batch_size,
+#         num_workers=num_workers,
+#         shuffle=True
+#     )
+
+
 import glob
 import numpy as np
 import torch
@@ -87,10 +151,12 @@ class CarlaDataset(Dataset):
 
         # Traverse through subdirectories
         for run_dir in glob.glob(os.path.join(self.data_dir, 'rc_data', 'run_*')):
-            # Load RGB images and Depth images
+            # Load RGB images
             rgb_dir = os.path.join(run_dir, 'rgb')
-            depth_dir = os.path.join(run_dir, 'disparity')  # Assuming depth images are in a 'disparity' folder
             self.img_list += glob.glob(os.path.join(rgb_dir, '*.jpg'))
+
+            # Load Depth images
+            depth_dir = os.path.join(run_dir, 'disparity')  # Adjust if depth images are in a different folder
             self.depth_list += glob.glob(os.path.join(depth_dir, '*.png'))
 
             # Load JSON files for actions
@@ -114,7 +180,7 @@ class CarlaDataset(Dataset):
         img_path = self.img_list[idx]
         img = read_image(img_path)
         img = img[:3, 120:600, 400:880]  # Crop the image
-        normalized_image = self.normalize(img.float() / 255.0)
+        normalized_image = img.float() / 255.0  # Normalize image to [0, 1]
 
         # Load the corresponding depth image
         depth_path = self.depth_list[idx]
