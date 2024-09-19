4794499df33ad5314ab3defac6d1635318046668
Adwait Kulkarni <adk1361@bu.edu>
2024-09-19 01:10:30 -0400

    imports added

--- Nerc_part/nerc_model.py
+++ Nerc_part/nerc_model.py
@@ -5,7 +5,8 @@ import torch.nn as nn
 import numpy as np
 import timm
 from torchvision.models.feature_extraction import create_feature_extractor
-
+from .Communication.Client.client import send_tensor, receive_tensor, run
+from .Communication.Server.server import CommunicationService, serve
 
 class CustomResNet18(nn.Module):
     def __init__(self):
--- Nerc_part/rc_orin_model.py
+++ Nerc_part/rc_orin_model.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import numpy as np
 import timm
 from torchvision.models.feature_extraction import create_feature_extractor
+from .Communication.Client.client import send_tensor, receive_tensor, run
 from .Communication.Server.server import CommunicationService, serve
 
 class CustomResNet18(nn.Module):
